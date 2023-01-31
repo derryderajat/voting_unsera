7d8cfb500a94b8a500ba00481967c5b507020021
derry derajat <derryderajat@mail.com>
2023-01-31 13:15:20 +0700

    1. Update model room, : default 0 for counting

--- models/activities.js
+++ models/activities.js
@@ -6,7 +6,7 @@ module.exports = (mongoose) => {
     mongoose.Schema({
       roomKey: { type: String, required: true },
       title: { type: String, required: true },
-      options: [{ title: String, count: Number }],
+      options: [{ title: String, count: { type: Number, default: 0 } }],
       votedBy: [{ name: String, tag: String }],
     })
   );
